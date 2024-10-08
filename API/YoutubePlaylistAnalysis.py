3ea4c1675c846355f2be476aaa89e295810a3ee8
deval <deval.patel5@dcmail.ca>
2024-10-08 01:18:23 -0400

    data exported to excel

--- API/YoutubePlaylistAnalysis.py
+++ API/YoutubePlaylistAnalysis.py
@@ -1,4 +1,6 @@
 import json
+import os
+import isodate
 
 import googleapiclient.discovery
 import pandas
@@ -42,11 +44,11 @@ class YoutubePlaylistAnalysis:
 
         return videoIds
 
-    def get_video_statistics(self, videolist=None):
+    def get_video_statistics(self, videolist=None) -> []:
+        data = []
         if videolist is None:
             raise ValueError('No videos found...')
         batch_size = 50
-        print(len(videolist))
         for i in range(0, len(videolist), batch_size):
             j = i + batch_size
             if j >= len(videolist):
@@ -57,6 +59,18 @@ class YoutubePlaylistAnalysis:
                 id=videoIds
             )
             response = request.execute()
+            for items in response['items']:
+                duration = items['contentDetails'].get('duration')
+                data.append([{
+                    'video_id': items.get('id'),
+                    'title': items['snippet'].get('title'),
+                    'tags': items['snippet'].get('tags'),
+                    'duration': isodate.parse_duration(duration).total_seconds(),
+                    'publised_date': items['snippet'].get('publishedAt'),
+                    'like_count': items['statistics'].get('likeCount'),
+                    'views_count': items['statistics'].get('viewCount')
+                }])
+        return data
 
 
 if __name__ == '__main__':
@@ -64,4 +78,32 @@ if __name__ == '__main__':
 
     playlistid = 'PLKnIA16_Rmvbr7zKYQuBfsVkjoLcJgxHH'
     video_ids = playlistInsights.get_video_ids(playlistid)
-    playlistInsights.get_video_statistics(video_ids)
+
+    # dataframe = pandas.DataFrame(playlistInsights.get_video_statistics(video_ids))
+    data_frame = pandas.DataFrame()
+    batch_size = 50
+    for i in range(0, len(video_ids), batch_size):
+        j = i + batch_size
+        if j >= len(video_ids):
+            j = len(video_ids) - 1
+        videoIds = ','.join(video_ids[i:j])
+        request = playlistInsights.get_request().videos().list(
+            part='snippet, contentDetails, statistics',
+            id=videoIds
+        )
+        response = request.execute()
+        for items in response['items']:
+            duration = items['contentDetails'].get('duration')
+            data = pandas.DataFrame([{
+                'video_id': items.get('id'),
+                'title': items['snippet'].get('title'),
+                'tags': items['snippet'].get('tags'),
+                'duration': isodate.parse_duration(duration).total_seconds(),
+                'publised_date': items['snippet'].get('publishedAt'),
+                'like_count': items['statistics'].get('likeCount'),
+                'views_count': items['statistics'].get('viewCount')
+            }])
+
+            data_frame = pandas.concat([data_frame,data], ignore_index=True)
+    print(data_frame)
+    data_frame.to_excel('C:/Users/deval/OneDrive/Desktop/Programing/data handling/Data Gathering Projects/videoInformation.xlsx')
